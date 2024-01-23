5c5d5616ee6500f62d2db3144ccf1a94d5e9f041
Bernardo de Oliveira Miranda Junior <bernardojunior@MacBook-Air-de-Bernardo.local>
2024-01-23 10:54:04 -0300

    feat(drink-menu): drink menu screen implemented

--- lib/components/drink_item.dart
+++ lib/components/drink_item.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class DrinkItem extends StatelessWidget {
-  const DrinkItem(
-      {Key? key,
-      required this.imageURI,
-      required this.itemTitle,
-      required this.itemPrice})
-      : super(key: key);
+  const DrinkItem({
+    Key? key,
+    required this.imageURI,
+    required this.itemTitle,
+    required this.itemPrice,
+  }) : super(key: key);
   final String imageURI;
   final String itemTitle;
   final String itemPrice;
@@ -20,6 +20,8 @@ class DrinkItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image(
+            width: double.infinity,
+            height: 94,
             image: AssetImage(imageURI),
             fit: BoxFit.cover,
           ),
@@ -30,9 +32,17 @@ class DrinkItem extends StatelessWidget {
               children: [
                 Text(
                   itemTitle,
-                  style: const TextStyle(fontSize: 16),
+                  style: const TextStyle(
+                    fontSize: 16,
+                    letterSpacing: 0.5,
+                  ),
                 ),
-                Text("R\$ $itemPrice"),
+                Text(
+                  "R\$ $itemPrice",
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
               ],
             ),
           ),
---
+++ lib/screens/drink_menu.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_layout/cardapio.dart';
+import 'package:responsive_layout/components/drink_item.dart';
+
+class DrinkMenu extends StatelessWidget {
+  const DrinkMenu({Key? key}) : super(key: key);
+
+  final List items = drinks;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: 16.0,
+              ),
+              child: Text(
+                'Bebidas',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Caveat',
+                  fontSize: 32,
+                ),
+              ),
+            ),
+          ),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              childCount: items.length,
+              (context, index) {
+                final drink = items[index];
+                return DrinkItem(
+                  imageURI: drink['image'],
+                  itemTitle: drink['name'],
+                  itemPrice: drink['price'],
+                );
+              },
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 158 / 194,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
